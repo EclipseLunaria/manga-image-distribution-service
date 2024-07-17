201e7e183804df5cc2e649324884d6b9d16b1a2c
Ecliptek-exe <dorsettj@gmail.com>
2024-07-17 14:48:49 -0700

    Started process of merging extract and image distro services

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,6 @@ import router from "./routes/routes";
 import express from "express";
 import process from "process";
 import commonRouter from "./routes/shared";
-import { closeBrowser } from "./utils/browserGlobal";
 
 const app = express();
 const port = process.env.PORT || 6967;
@@ -17,14 +16,3 @@ const server = app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
 // Handle server shutdown
-const gracefulShutdown = async () => {
-  console.log('Shutting down server...');
-  await closeBrowser();
-  server.close(() => {
-    console.log('Server closed');
-    process.exit(0);
-  });
-};
-
-process.on('SIGTERM', gracefulShutdown);
-process.on('SIGINT', gracefulShutdown);
