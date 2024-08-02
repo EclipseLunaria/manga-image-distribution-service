ba7ca78e35b745834e77f5b8cefc43a4038fe22f
Jackson Dorsett <dorsettj@gmail.com>
2024-08-01 19:00:11 -0700

    changed route scheme

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -3,11 +3,10 @@ import { getChapterContent, checkChapterExists } from "../controllers";
 import { extractChapter } from "../controllers";
 const router = Router();
 
-router.get("/fetch/manga-:mangaId/chapter-:chapterId", getChapterContent);
+router.get("/fetch/manga/:mangaId/chapter/:chapterId", getChapterContent);
 
-router.get("/exists/manga-:mangaId/chapter-:chapterId", checkChapterExists);
+router.get("/exists/manga/:mangaId/chapter/:chapterId", checkChapterExists);
 
-router.get("/extract/manga-:mangaId/chapter-:chapterId", extractChapter);
-
+router.get("/extract/manga/:mangaId/chapter/:chapterId", extractChapter);
 
 export default router;
